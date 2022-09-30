ba3adbeb97fc0095fca410e4a2c3c1b65b283396
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-09-30 17:59:45 -0300

    update main

--- main.py
+++ main.py
@@ -1,1 +1,86 @@
 #Programa de criar uma conta
+
+#Regras Da parte de criar conta
+
+def Regrasnome(): 
+    nome = str(input('Seu nome :'))
+    nome = nome.replace('', ' ')
+    if len(nome) > 2:
+        print('Seu nome está muito grande refaça ele novamente')
+        pass
+
+def Regrasnick():
+    nick = nick.replace('', ' ').strip()
+
+def Regrasemail():
+    email = email.replace('', ' ').strip()
+
+def Regrassenha():
+    senha = senha.replace('', ' ').strip()
+
+
+#Programa principal
+
+while True:
+    print('-=' * 25)
+    print(' ' * 10, 'Bem vindo ao painel da Takern')
+    print('-=' * 25)
+    print('Você deseja:')
+    print('[1] Criar conta')
+    print('[2] Entrar em uma conta')
+    print('[3] Fechar o programa')
+
+    escolha = str(input(''))
+    escolha = escolha.replace('', ' ').strip()
+
+
+    if not escolha.isnumeric():
+        print('Você não digitou um número')
+        print('Você precisa digitar um número!')
+        print('Esse numero não existe deseja voltar? (s/n)')
+        voltar = str(input('')).strip().lower().replace('', ' ') 
+        if voltar == 's':
+            pass
+        elif voltar == 'n':
+            print('Volte sempre!')
+            break
+        else:
+            print('Escreva direito!')
+            break
+        
+        
+    escolha = int(escolha)
+
+    if escolha == 1:
+        while True:
+        
+                Regrasnome()
+            
+                nick = str(input('Crie seu Nick Name :'))
+                email = str(input('Seu Email :'))
+                senha = str(input('Crie uma Senha :'))
+
+                break
+
+
+
+    elif escolha == 2:
+        pass
+
+    elif escolha == 3:
+        break
+
+    else:
+        print('Esse numero não existe deseja voltar? (s/n)')
+        voltar = str(input('')).upper()
+        
+        if voltar == 'S':
+            pass
+
+        elif voltar == 'N':
+            print('Volte sempre!')
+            break
+
+        else:
+            print('Escreva direito!')
+            break
