988ca864c70d5e28a6abba099168d7604be29cc0
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-11-16 23:48:42 -0300

    update

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 #import's
 from replit import clear
+
+
+
+
+
+#Programa principal
+
+while True:
+    clear()
+    print('-=' * 25)
+    print(' ' * 10, 'Bem vindo ao painel da Takern')
+    print('-=' * 25)
+    print('Você deseja:')
+    print('[1] Criar conta')
+    print('[2] Entrar em uma conta')
+    print('[3] Fechar o programa')
+    escolha = str(input('R:'))
+    escolha = escolha.replace('', ' ').strip() #tirar todos os espaços
+
+    if escolha == '1': # criar conta
+        while True:
+            clear()              
+            break
