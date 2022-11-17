476ed5323749c84e2a5bf8ff7e3802464b8bf99f
Gabriel-FerreirasSantos <gabriel.ruasmil@gmail.com>
2022-11-17 00:16:23 -0300

    update

--- func/func_nome/main_func_nome.py
+++ func/func_nome/main_func_nome.py
@@ -6,7 +6,7 @@ def Regrasnome():
         nome = str(input('Seu nome :'))
         nome = nome.replace('', ' ')
         if len(nome) > 10:
-            clear
+            clear()
             print('Seu nome está muito grande refaça ele novamente')
         else:
             break
